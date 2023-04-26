279f12387be7b763ab05f4895fe823167c4f0e51
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2023-04-27 01:21:07 +0530

    Test the hasura engine api through code

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: "my-graphql-app",
+      url: "http://localhost:8080/v1/graphql",
+    },
+  },
+};
--- test/api.test.js
+++ test/api.test.js
@@ -1,1 +1,41 @@
+const { gql } = require("graphql-request");
+
 API_URL = "http://localhost:8080/v1/graphql";
+
+async function makeQuery(query, variables) {
+  return await fetch("http://localhost:8080/v1/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      //variables,
+    }),
+  });
+}
+
+async function streamToString(stream) {
+  // lets have a ReadableStream as a stream variable
+  const chunks = [];
+
+  for await (const chunk of stream) {
+    chunks.push(Buffer.from(chunk));
+  }
+
+  return Buffer.concat(chunks).toString("utf-8");
+}
+
+test("can query the database", async () => {
+  const res = await makeQuery(
+    gql`
+      query MyQuery {
+        tasks {
+          id
+        }
+      }
+    `
+  );
+
+  console.log(JSON.stringify(await res.json()));
+});
