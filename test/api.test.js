6b9c4cf400ab007681709faaaba86ba7ecb3d743
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2023-04-28 02:45:23 +0530

    Use hfh to add relationships and authorisation to sample app, add admin secret to the hasura server

--- test/api.test.js
+++ test/api.test.js
@@ -2,15 +2,16 @@ const { gql } = require("graphql-request");
 
 API_URL = "http://localhost:8080/v1/graphql";
 
-async function makeQuery(query, variables) {
+async function makeQuery(query, variables, headers = {}) {
   return await fetch("http://localhost:8080/v1/graphql", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      ...headers,
     },
     body: JSON.stringify({
       query,
-      //variables,
+      variables,
     }),
   });
 }
@@ -26,7 +27,7 @@ async function streamToString(stream) {
   return Buffer.concat(chunks).toString("utf-8");
 }
 
-test("can query the database", async () => {
+test("queries without any authn fail", async () => {
   const res = await makeQuery(
     gql`
       query MyQuery {
@@ -35,6 +36,24 @@ test("can query the database", async () => {
         }
       }
     `
+  );
+
+  expect((await res.json()).errors[0].message).toEqual(
+    "x-hasura-admin-secret/x-hasura-access-key required, but not found"
+  );
+});
+
+test("can query the database", async () => {
+  const res = await makeQuery(
+    gql`
+      query MyQuery {
+        tasks {
+          id
+        }
+      }
+    `,
+    undefined,
+    { "X-Hasura-Admin-Secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET }
   );
 
   expect((await res.json()).data.tasks).not.toBeUndefined();
